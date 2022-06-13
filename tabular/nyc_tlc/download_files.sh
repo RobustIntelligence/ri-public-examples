ad56066d75ba2d2d73dddbc466915a3b020466c3
Jerry Liu <jerry@robustintelligence.com>
2022-06-13 13:19:46 -0700

    cr

--- tabular/nyc_tlc/download_files.sh
+++ tabular/nyc_tlc/download_files.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-wget https://www.dropbox.com/s/80lk5l2ycsu432w/eval.csv?dl=1 -O $1/eval.csv
-wget https://www.dropbox.com/s/ioac4g66l3gunzt/ref.csv?dl=1 -O $1/ref.csv
-wget https://www.dropbox.com/s/riq51qxgjw4dfne/test.csv?dl=0 -O $1/test.csv
-wget https://www.dropbox.com/s/cvk8q1ce3ad98w0/model.catb?dl=1 -O $1/model/model_extras/model.catb
+
+# check that output directory is supplied
+if [[ $1 -eq 0 ]] ; then
+    echo 'Must specify output directory e.g. `./download_files.sh <output_dir>`'
+    exit 1
+fi
+wget https://www.dropbox.com/s/qxvp1nxzautn0e0/eval.csv?dl=1 -O $1/eval.csv
+wget https://www.dropbox.com/s/ykhvr387m94v0cn/ref.csv?dl=1 -O $1/ref.csv
+wget https://www.dropbox.com/s/1afv0x225rm0oc6/test.csv?dl=1 -O $1/test.csv
+wget https://www.dropbox.com/s/iuwmlaxcl3xghwg/model.catb?dl=1 -O $1/model/model_extras/model.catb
