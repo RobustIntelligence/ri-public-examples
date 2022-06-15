2a954619a0156e5215c935bdaec89df1aca94871
Jerry Liu <jerry@robustintelligence.com>
2022-06-15 15:56:21 -0700

    add urls/configs for awa dataset

--- download_files.py
+++ download_files.py
@@ -10,14 +10,14 @@ def _fetch_urls_and_outpaths(file_path: Path) -> List[Tuple[str, str]]:
     urls_and_outpaths = []
     with open(file_path, "r") as fp:
         for line in fp:
-            tokens = tuple([t.strip() for t in line.split(',')])
+            tokens = [t.strip() for t in line.split(',')]
             if len(tokens) != 2:
                 raise Exception(
                     "Number of comma-separated values should be 2. "
                     f"Actual: {len(tokens)}"
                 )
 
-            urls_and_outpaths.append(tokens)
+            urls_and_outpaths.append((tokens[0], Path(tokens[1])))
     return urls_and_outpaths
 
 
@@ -32,6 +32,17 @@ def _download_files(urls_and_outpaths: List[Tuple[str, str]], parent_dir: Path) 
         if not outdir.exists():
             os.makedirs(outdir)
         subprocess.run(["wget", url, "-O", full_outpath])
+        # if downloading a zip file, unzip
+        if outpath.suffix == ".zip":
+            # unzip in parent directory
+            subprocess.run(["unzip", "-o", full_outpath, "-d", outdir])
+            # assert that the unzipped directory has same name as .zip file
+            full_outpath_unzip = outdir / (outpath.stem)
+            if not full_outpath_unzip.exists():
+                raise Exception(
+                    "The unzipped directory must exist at following output path: "
+                    f"{full_outpath_unzip}. Original zip path: {full_outpath}."
+                )
 
 
 def download_files(project_dir: Path) -> None:
