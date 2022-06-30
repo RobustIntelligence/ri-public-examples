95b12056306d2b66c24a389eb0f62aa4a3845fa1
Jerry Liu <jerry@robustintelligence.com>
2022-06-29 22:47:26 -0700

    rfc: proposal to be able to download other files besides data/models

--- ri_public_examples/download_files.py
+++ ri_public_examples/download_files.py
@@ -10,21 +10,28 @@ GITHUB_URL = "https://raw.githubusercontent.com/RobustIntelligence/ri-public-exa
 DEFAULT_CONFIG_PATH = "configs/stress_test_config.json"
 
 
-def _download_file(remote_path: str, local_path: Path) -> None:
+def _download_file(remote_path: str, local_path: Path) -> int:
     """Downloads file to local path."""
     if not local_path.parent.exists():
         local_path.parent.mkdir(parents=True)
     r = requests.get(remote_path)
-    with open(local_path, 'wb') as f:
-        f.write(r.content)
+    if r.status_code == 200:
+        with open(local_path, 'wb') as f:
+            f.write(r.content)
+    
+    return r.status_code
 
-def _fetch_urls_and_outpaths(remote_file_path: str, out_dir: Path) -> List[Tuple[str, Path]]:
+def _fetch_urls_and_outpaths(
+    remote_file_path: str, out_dir: Path
+) -> Optional[List[Tuple[str, Path]]]:
     """Fetch urls and output paths from text file."""
     # TODO: add data_urls/configs to package data directly
     # first download remote file path
     local_path = out_dir / Path(remote_file_path).name
     
-    _download_file(remote_file_path, local_path)
+    status_code = _download_file(remote_file_path, local_path)
+    if status_code != 200:
+        return None
     
     urls_and_outpaths = []
     with open(local_path, "r") as fp:
@@ -90,10 +97,15 @@ def download_files(
     # load in URL's and output paths
     data_urls_and_outpaths = _fetch_urls_and_outpaths(project_dir + "/data_urls.csv", out_dir)
     model_urls_and_outpaths = _fetch_urls_and_outpaths(project_dir + "/model_urls.csv", out_dir)
+    other_urls_and_outpaths = _fetch_urls_and_outpaths(project_dir + "/other_urls.csv", out_dir)
 
     # download files
-    _download_files(data_urls_and_outpaths, out_dir / "data")
-    _download_files(model_urls_and_outpaths, out_dir / "models")
+    if data_urls_and_outpaths is not None:
+        _download_files(data_urls_and_outpaths, out_dir / "data")
+    if model_urls_and_outpaths is not None:
+        _download_files(model_urls_and_outpaths, out_dir / "models")
+    if other_urls_and_outpaths is not None:
+        _download_files(other_urls_and_outpaths, out_dir / "other")
 
     # download configs if config_paths specified
     if config_files is not None:
