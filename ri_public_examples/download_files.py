cbe434fa1b47dff99cc774739b310373970f7356
Jerry Liu <jerryjliu98@gmail.com>
2022-07-22 13:00:16 -0700

    Revert "[RFC] proposal to be able to download other files besides data/models (#34)" (#35)
    This reverts commit 101ea7270ac04602f4ee1935542ce628ec97991a.
    
    Co-authored-by: Jerry Liu <jerry@robustintelligence.com>

--- ri_public_examples/download_files.py
+++ ri_public_examples/download_files.py
@@ -4,42 +4,28 @@ from typing import List, Optional, Tuple
 from zipfile import ZipFile
 from pathlib import Path
 import requests
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 GITHUB_URL = "https://raw.githubusercontent.com/RobustIntelligence/ri-public-examples/master/"
 DEFAULT_CONFIG_PATH = "configs/stress_test_config.json"
 
 
-def _download_file(remote_path: str, local_path: Path) -> int:
-    """Downloads file to local path and returns status code of request."""
+def _download_file(remote_path: str, local_path: Path) -> None:
+    """Downloads file to local path."""
     if not local_path.parent.exists():
         local_path.parent.mkdir(parents=True)
     r = requests.get(remote_path)
-    if r.status_code == 200:
-        with open(local_path, 'wb') as f:
-            f.write(r.content)
-    else:
-        logger.warning(
-            f"Invalid status code: {r.status_code} for request {remote_path}"
-        )
+    with open(local_path, 'wb') as f:
+        f.write(r.content)
 
-    return r.status_code
-
-def _fetch_urls_and_outpaths(
-    remote_file_path: str, out_dir: Path
-) -> Optional[List[Tuple[str, Path]]]:
+def _fetch_urls_and_outpaths(remote_file_path: str, out_dir: Path) -> List[Tuple[str, Path]]:
     """Fetch urls and output paths from text file."""
     # TODO: add data_urls/configs to package data directly
     # first download remote file path
     local_path = out_dir / Path(remote_file_path).name
-
-    status_code = _download_file(remote_file_path, local_path)
-    if status_code != 200:
-        return None
-
+    
+    _download_file(remote_file_path, local_path)
+    
     urls_and_outpaths = []
     with open(local_path, "r") as fp:
         for line in fp:
@@ -104,15 +90,10 @@ def download_files(
     # load in URL's and output paths
     data_urls_and_outpaths = _fetch_urls_and_outpaths(project_dir + "/data_urls.csv", out_dir)
     model_urls_and_outpaths = _fetch_urls_and_outpaths(project_dir + "/model_urls.csv", out_dir)
-    other_urls_and_outpaths = _fetch_urls_and_outpaths(project_dir + "/other_urls.csv", out_dir)
 
     # download files
-    if data_urls_and_outpaths is not None:
-        _download_files(data_urls_and_outpaths, out_dir / "data")
-    if model_urls_and_outpaths is not None:
-        _download_files(model_urls_and_outpaths, out_dir / "models")
-    if other_urls_and_outpaths is not None:
-        _download_files(other_urls_and_outpaths, out_dir / "other")
+    _download_files(data_urls_and_outpaths, out_dir / "data")
+    _download_files(model_urls_and_outpaths, out_dir / "models")
 
     # download configs if config_paths specified
     if config_files is not None:
