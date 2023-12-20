1c2d48793209bb2ba53370605bfe83afb07aab76
Massimo Aufiero <massimo@robustintelligence.com>
2023-12-20 15:20:31 -0500

    undo test change

--- ri_public_examples/download_files.py
+++ ri_public_examples/download_files.py
@@ -9,7 +9,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-GITHUB_URL = "https://raw.githubusercontent.com/RobustIntelligence/ri-public-examples/419c58dcbe308a0f6d25415a7ab5351608bca41b/"
+GITHUB_URL = "https://raw.githubusercontent.com/RobustIntelligence/ri-public-examples/master/"
 DEFAULT_CONFIG_PATH = "configs/stress_test_config.json"
 
 
